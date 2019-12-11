93c7a23642b6aabc4175723aa8a39c3b2727f775
Gergő Zilizi <zilizigergo@gmail.com>
2019-12-11 17:31:39 +0100

    Components, Kits (#2)

---
+++ MongoProject.WebApp/Data/Models/Components/BLDCMotor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoProject.WebApp.Data.Models
+{
+    public class BLDCMotor : Component
+    {
+        public double Shaftdiameter { get; set; }
+        public double RPMV { get; set; }
+        public double Voltagerating { get; set; }
+    }
+}
---
+++ MongoProject.WebApp/Data/Models/Components/Bolt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoProject.WebApp.Data.Models
+{
+    public class Bolt : Component
+    {
+        public double Size { get; set; }
+        public double Length { get; set; }
+        public double Head { get; set; }
+    }
+}
